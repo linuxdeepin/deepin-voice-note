fbefa273fc5f2acca3e42f26480c4ac8da49a0c1
wujian <wujian@uniontech.com>
2020-11-30 14:27:29 +0800

    feat: 分离语音插入和语音初始化逻辑，修复pastehtml时无法主动插入换行符问题
     分离语音插入和语音初始化逻辑，修复pastehtml时无法主动插入换行符问题
    
    Log:
    
    Bug:

--- assets/web/index.js
+++ assets/web/index.js
@@ -11,15 +11,48 @@
 //js 调用c++接口
 
 
+var h5Tpl  = `
+<div class="li" contenteditable="false" jsonKey="{{jsonValue}}">	
+    <div class="demo" >
+        <div class="left">
+        <div class="btn play"></div>
+        </div>
+        <div class="right">
+            <div class="lf">
+                <div class="title">{{title}}</div>
+                <div class="minute padtop">{{createTime}}</div>
+            </div>
+            <div class="lr">
+                <div class="icon">
+                <div class="wifi-symbol">
+                    <div class="wifi-circle first"></div>
+                    <div class="wifi-circle second"></div>
+                    <div class="wifi-circle third"></div>
+                    <div class="wifi-circle four"></div>
+                </div>
+                </div>
+                <div class="time padtop">{{transSize}}</div>
+            </div>
+            <div class="transBtn"></div>
+        </div>
+    </div>
+    <div class="translate">
+        {{#if text}}
+        <div>{{text}}</div>
+        {{/if}}
+    </div>
+</div>`;
 
 var webobj;    //js与qt通信对象
 var activeVoice;  //当前正操作的语音对象
+var activeTransVoice; 
+
 $('#summernote').summernote({
-    height: 300,                 // set editor height
+    // height: 300,                 // set editor height
     minHeight: null,             // set minimum height of editor
     maxHeight: null,             // set maximum height of editor
     focus: true,                  // set focus to editable area after initializin
-    airMode: true,
+    // airMode: true,
     disableDragAndDrop: true,
     shortcuts:false,
 }); 
@@ -38,16 +71,23 @@ $('body').on('click', '.btn', function (e) {
     e.stopPropagation();
     var curVoice = $(this).parents('.li:first');
     var jsonString = curVoice.attr('jsonKey');
-    var bIsSame = $(this).hasClass('active');
+    var bIsSame = $(this).hasClass('now');
     var curBtn = $(this);
-    $('.btn').removeClass('active');
+    $('.btn').removeClass('now');
     activeVoice = curBtn;
-    activeVoice.addClass('active');
+    activeVoice.addClass('now');
     
     webobj.jsCallPlayVoice(jsonString, bIsSame, function (state) {
         //TODO 录音错误处理
     });
-    //播放
+})
+
+//语音转文字按钮
+$('body').on('click', '.transBtn', function (e) {
+    console.log('------trans click...');
+    var jsonString = $(this).parents('.li:first').attr('jsonKey');
+    webobj.jsCallPopVoiceMenu(jsonString);
+    activeTransVoice = $(this).parents('.li:first');
 })
 
 //播放状态，0,播放中，1暂停中，2.结束播放
@@ -61,103 +101,12 @@ function toggleState(state) {
     }
     else{
         activeVoice.removeClass('pause').addClass('play');
-        $('.btn').removeClass('active');
+        $('.btn').removeClass('now');
     }
 }
 
-//type:1 初始化  2 插入
-function init(type, arr) {
-    console.log('=============>',arr);
-    var tpl = $("#voiceTemplate").html();
-    var template = Handlebars.compile(tpl);
-    var html = '';
-    var voiceHtml;
-    var txtHtml;
-    // if (type == 1){
-    //     html += '<p><br></p>';
-    // }
-    arr.noteDatas.forEach((item, index) => {
-        //false: txt
-        if (item.type == 1) {
-            if (item.text == '')
-            {
-                txtHtml = '<p><br></p>';
-            }
-            else{
-                txtHtml = '<p>' + item.text +'</p>';
-            }
-            html += txtHtml;
-        }
-        //true: voice
-        else{
-            //将json内容当其属性与标签绑定
-            var strItem = JSON.stringify(item);
-            item.jsonValue = strItem;
-            voiceHtml = template(item);
-            html += voiceHtml
-        }
-    })
-    html += '<p><br></p>';
-    if (type == 1){
-        $('#summernote').summernote('code', html);
-    }
-    else{
-        $('#summernote').summernote('pasteHTML', html);
-    }
-}
-
-function fnInit(text, type) {
-    var initText = JSON.parse(text);
-    var newText = initText.noteDatas;
-
-    //解决异步更新数据页面接收问题
-    //获取转换时间
-    function fnAsyncTime(item) {
-        return new Promise(function (resolve, reject) {
-            webobj.jsCallGetVoiceTime(item.createTime, function (time) {
-                // item.createTime = time;
-                item.transTime = time;
-                resolve();
-            });
-        })
-    }
-    //获取转换时长
-    function fnAsyncSize(item){
-        return new Promise(function (resolve, reject) {
-            webobj.jsCallGetVoiceSize(item.voiceSize, function (vSize) {
-                // item.voiceSize = vSize;
-                item.transSize = vSize;
-                resolve()
-            })
-        })
-    }
-
-
-    Promise.all(
-        newText.map(item => {
-            return new Promise(async (resolve, reject) => {
-                await fnAsyncTime(item);
-                await fnAsyncSize(item);
-                resolve()
-            })
-        })
-    ).then(() => {
-        initText.noteDatas = newText;
-        init(type, initText)
-    })
-}
-
-
-//初始化数据
-var initData = function (text) {
-    console.log('--initdata---',text);
-    fnInit(text, 1)
-}
-
-//录音插入数据
-var insertVoiceItem = function (text) {
-    console.log('--insertVoiceItem---',text);
-    fnInit(text, 2);
+function getHtml(){
+    return $('#summernote').summernote('code');
 }
 
 new QWebChannel(qt.webChannelTransport,
@@ -170,13 +119,78 @@ new QWebChannel(qt.webChannelTransport,
         webobj.callJsInsertVoice.connect(insertVoiceItem);
         webobj.callJsSetPlayStatus.connect(toggleState);
         webobj.callJsSetHtml.connect(setHtml);
+        webobj.callJsSetVoiceText.connect(setVoiceText);
 })
 
-function getHtml(){
-    return $('#summernote').summernote('code');
+//初始化数据 
+function initData(text) {
+    var arr = JSON.parse(text);
+    console.log('=============>',arr);
+    var html = '';
+    var voiceHtml;
+    var txtHtml;
+
+    arr.noteDatas.forEach((item, index) => {
+        //false: txt
+        if (item.type == 1) {
+            console.log('---txt---');
+            if (item.text == '')
+            {
+                txtHtml = '<p><br></p>';
+            }
+            else{
+                txtHtml = '<p>' + item.text +'</p>';
+            }
+            html += txtHtml;
+        }
+        //true: voice
+        else{
+            console.log('---voice---');
+            voiceHtml = transHtml(item);
+            html += voiceHtml;
+        }
+    })
+
+    $('#summernote').summernote('code', html);
 }
 
+//录音插入数据
+function insertVoiceItem(text) {
+    console.log('--insertVoiceItem---',text);
+    var arr = JSON.parse(text);
+    var voiceHtml = transHtml(arr);
+    $('#summernote').summernote('pasteHTML', voiceHtml);
+}
+
+//设置整个html内容
 function setHtml(html){
     console.log('--setHtml---',html);
     $('#summernote').summernote('code',html);
+}
+
+//设置录音转文字内容 flag: 0: 转换过程中 提示性文本（＂正在转文字中＂)１:结果 文本,空代表转失败了
+function setVoiceText(text,flag){
+    console.log('----voice text----');
+    // activeTransVoice.
+    if (activeTransVoice)
+    {
+        if (flag)
+        {
+            if(!text) return;
+        activeTransVoice.find('.translate').html('<div>'+text+'</div>');
+        }
+        else
+        {
+            activeTransVoice.find('.translate').html('<p class="noselect">'+text+'</p>');
+        }
+    }
+}
+
+function transHtml(json){
+    //将json内容当其属性与标签绑定
+    var strItem = JSON.stringify(json);
+    json.jsonValue = strItem;
+    var template = Handlebars.compile(h5Tpl);
+    var retHtml =  template(json);
+    return retHtml;
 }
