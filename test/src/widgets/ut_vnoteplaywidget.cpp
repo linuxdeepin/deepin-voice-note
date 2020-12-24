cd9e06c11585cf896577d36ece41672978031b58
zhangteng <zhangteng@uniontech.com>
2020-12-24 17:07:13 +0800

    chore: 添加ut测试用例
     添加ut测试用例
    
    Log: 添加ut测试用例
    Change-Id: I5d6d9f78756db679a84373f0e159e12b0f894463

---
+++ test/src/widgets/ut_vnoteplaywidget.cpp
@@ -0,0 +1,62 @@
+/*
+* Copyright (C) 2019 ~ 2020 Deepin Technology Co., Ltd.
+*
+* Author:     zhangteng <zhangteng@uniontech.com>
+* Maintainer: zhangteng <zhangteng@uniontech.com>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* any later version.
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "ut_vnoteplaywidget.h"
+#include "vnoteplaywidget.h"
+#include "vnoteitem.h"
+#include "vnote2siconbutton.h"
+#include "utils.h"
+
+ut_vnoteplaywidget_test::ut_vnoteplaywidget_test()
+{
+}
+
+void ut_vnoteplaywidget_test::SetUp()
+{
+    m_vnoteplaywidget = new VNotePlayWidget;
+}
+
+void ut_vnoteplaywidget_test::TearDown()
+{
+    delete m_vnoteplaywidget;
+}
+
+TEST_F(ut_vnoteplaywidget_test, onVoicePlayPosChange)
+{
+    m_vnoteplaywidget->onVoicePlayPosChange(24);
+}
+
+TEST_F(ut_vnoteplaywidget_test, setVoiceBlock)
+{
+    //    VNoteItem tmpNote;
+    //    tmpNote.folderId = 2;
+    //    tmpNote.noteType = VNoteItem::VNT_Voice;
+    //    VNVoiceBlock *ptrBlock = new VNVoiceBlock;
+    //    m_vnoteplaywidget->setVoiceBlock(ptrBlock);
+}
+
+TEST_F(ut_vnoteplaywidget_test, playVideo)
+{
+    m_vnoteplaywidget->onSliderPressed();
+    m_vnoteplaywidget->initPlayer();
+    m_vnoteplaywidget->onPlayBtnClicked();
+    m_vnoteplaywidget->onSliderReleased();
+    m_vnoteplaywidget->onPauseBtnClicked();
+    m_vnoteplaywidget->onCloseBtnClicked();
+    m_vnoteplaywidget->getPlayerStatus();
+    m_vnoteplaywidget->getVoiceData();
+}
---
+++ test/src/widgets/ut_vnoterecordwidget.cpp
@@ -0,0 +1,70 @@
+/*
+* Copyright (C) 2019 ~ 2020 Deepin Technology Co., Ltd.
+*
+* Author:     zhangteng <zhangteng@uniontech.com>
+* Maintainer: zhangteng <zhangteng@uniontech.com>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* any later version.
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "ut_vnoterecordwidget.h"
+#include "vnoterecordwidget.h"
+#include "utils.h"
+
+ut_vnoterecordwidget_test::ut_vnoterecordwidget_test()
+{
+}
+
+void ut_vnoterecordwidget_test::SetUp()
+{
+    m_vnoterecordwidget = new VNoteRecordWidget;
+}
+
+void ut_vnoterecordwidget_test::TearDown()
+{
+    delete m_vnoterecordwidget;
+}
+
+TEST_F(ut_vnoterecordwidget_test, startRecord)
+{
+    QAudioFormat audioformat;
+    const QByteArray data;
+    audioformat.setCodec("audio/pcm");
+    //通道，采样率
+    audioformat.setChannelCount(2);
+    audioformat.setSampleRate(44100);
+    //lamemp3enc 编码器插件格式为S16LE
+    audioformat.setByteOrder(QAudioFormat::LittleEndian);
+    audioformat.setSampleType(QAudioFormat::UnSignedInt);
+    audioformat.setSampleSize(16);
+    QAudioBuffer buffer(data, audioformat);
+    m_vnoterecordwidget->onAudioBufferProbed(buffer);
+    m_vnoterecordwidget->startRecord();
+    m_vnoterecordwidget->onRecordDurationChange(4);
+    m_vnoterecordwidget->onPauseRecord();
+    m_vnoterecordwidget->onContinueRecord();
+    m_vnoterecordwidget->stopRecord();
+}
+
+TEST_F(ut_vnoterecordwidget_test, getRecordPath)
+{
+    m_vnoterecordwidget->getRecordPath();
+}
+
+TEST_F(ut_vnoterecordwidget_test, setAudioDevice)
+{
+    m_vnoterecordwidget->setAudioDevice("test");
+}
+
+TEST_F(ut_vnoterecordwidget_test, onGstreamerFinshRecord)
+{
+    m_vnoterecordwidget->onGstreamerFinshRecord();
+}
