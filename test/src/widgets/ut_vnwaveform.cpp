eab0b8815195ec44779a1d616a28a098c0d3c919
zhangteng <zhangteng@uniontech.com>
2020-12-24 13:40:41 +0800

    chore:添加vnwaveform类ut测试用例。
    添加vnwaveform类ut测试用例。
    
    Log: 添加vnwaveform类ut测试用例。
    Change-Id: I14619a479f70263e25805ffc94572e7a4286a0d9

--- test/src/widgets/ut_vnwaveform.cpp
+++ test/src/widgets/ut_vnwaveform.cpp
@@ -32,3 +32,83 @@ TEST_F(ut_vnwaveform_test, onAudioBufferProbed)
     buffer.format().setSampleSize(16);
     vnwaveform.onAudioBufferProbed(buffer);
 }
+
+TEST_F(ut_vnwaveform_test, paintEvent)
+{
+    VNWaveform vnwaveform;
+    vnwaveform.m_audioScaleSamples.push_back(1.1);
+    vnwaveform.m_audioScaleSamples.push_back(1.2);
+    vnwaveform.m_audioScaleSamples.push_back(1.3);
+    vnwaveform.m_audioScaleSamples.push_back(1.4);
+    vnwaveform.m_maxShowedSamples = 4;
+    vnwaveform.paintEvent(nullptr);
+}
+
+TEST_F(ut_vnwaveform_test, resizeEvent)
+{
+    VNWaveform vnwaveform;
+    QSize size1(10, 12);
+    QSize size2(5, 6);
+    QResizeEvent *event = new QResizeEvent(size1, size2);
+    vnwaveform.resizeEvent(event);
+}
+
+TEST_F(ut_vnwaveform_test, getBufferLevels)
+{
+    VNWaveform vnwaveform;
+    QAudioFormat audioformat;
+    QVector<qreal> scaleSamples;
+    const QByteArray data;
+    qreal frameGain = 0;
+    audioformat.setCodec("audio/pcm");
+    //通道，采样率
+    audioformat.setChannelCount(2);
+    audioformat.setSampleRate(44100);
+    //lamemp3enc 编码器插件格式为S16LE
+    audioformat.setByteOrder(QAudioFormat::LittleEndian);
+    audioformat.setSampleType(QAudioFormat::UnSignedInt);
+    audioformat.setSampleSize(16);
+    QAudioBuffer buffer(data, audioformat);
+    vnwaveform.getBufferLevels(buffer, scaleSamples, frameGain);
+
+    audioformat.setSampleSize(32);
+    QAudioBuffer buffer1(6, audioformat);
+    vnwaveform.getBufferLevels(buffer1, scaleSamples, frameGain);
+
+    audioformat.setSampleSize(8);
+    QAudioBuffer buffer2(6, audioformat);
+    vnwaveform.getBufferLevels(buffer2, scaleSamples, frameGain);
+
+    audioformat.setSampleType(QAudioFormat::Float);
+    audioformat.setSampleSize(32);
+    QAudioBuffer buffer3(6, audioformat);
+    vnwaveform.getBufferLevels(buffer3, scaleSamples, frameGain);
+
+    audioformat.setSampleType(QAudioFormat::SignedInt);
+    audioformat.setSampleSize(32);
+    QAudioBuffer buffer4(6, audioformat);
+    vnwaveform.getBufferLevels(buffer4, scaleSamples, frameGain);
+
+    audioformat.setSampleSize(16);
+    QAudioBuffer buffer5(6, audioformat);
+    vnwaveform.getBufferLevels(buffer5, scaleSamples, frameGain);
+
+    audioformat.setSampleSize(8);
+    QAudioBuffer buffer6(6, audioformat);
+    vnwaveform.getBufferLevels(buffer6, scaleSamples, frameGain);
+}
+
+TEST_F(ut_vnwaveform_test, getPeakValue)
+{
+    VNWaveform vnwaveform;
+    QAudioFormat audioformat;
+    audioformat.setCodec("audio/pcm");
+    //通道，采样率
+    audioformat.setChannelCount(2);
+    audioformat.setSampleRate(44100);
+    //lamemp3enc 编码器插件格式为S16LE
+    audioformat.setByteOrder(QAudioFormat::LittleEndian);
+    audioformat.setSampleType(QAudioFormat::Unknown);
+    audioformat.setSampleSize(16);
+    vnwaveform.getPeakValue(audioformat);
+}
