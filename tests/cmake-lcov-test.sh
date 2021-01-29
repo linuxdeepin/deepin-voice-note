affe766360fd56310e27f21539b7b3ce7afbb421
UT000454 <liuyanga@uniontech.com>
2021-01-29 11:01:53 +0800

    chore: 修改测试文件夹名称test为tests
     修改测试文件夹名称test为tests
    
    Log: 修改测试文件夹名称test为tests
    Change-Id: Ibdb408a530c1d10144bd2214fb40ab2420c6335e

--- tests/cmake-lcov-test.sh
+++ tests/cmake-lcov-test.sh
@@ -15,14 +15,14 @@ pathname=$(find . -name obj*)
 
 echo $pathname
 
-cd $pathname/test
+cd $pathname/tests
 
 mkdir -p coverage
 
 lcov --directory ../ --capture --output-file ./coverage/coverage.info
 
 #以下几行是过滤一些我们不感兴趣的文件的覆盖率信息，各模块根据自模块实际情况填写过滤信息
-lcov --remove ./coverage/coverage.info '*/${project_name}_test_autogen/*' '*/${project_name}_autogen/*' '*/usr/include/*' '*/dbuslogin1manager*' '*obj-x86_64-linux-gnu/*' '*/test/*' '*/task/*' '*/importolddata/*' '*/vnoteapplication.cpp*' '*/db/*' '*/views/*delegate*' -o ./coverage/coverage.info
+lcov --remove ./coverage/coverage.info '*/${project_name}_test_autogen/*' '*/${project_name}_autogen/*' '*/usr/include/*' '*/dbuslogin1manager*' '*obj-x86_64-linux-gnu/*' '*/tests/*' '*/task/*' '*/importolddata/*' '*/vnoteapplication.cpp*' '*/db/*' '*/views/*delegate*' -o ./coverage/coverage.info
 
 mkdir ../report
 genhtml -o ../report ./coverage/coverage.info
