e0c41a16f2a5c00a08940d3035f80a34d186afc1
UT000454 <liuyanga@uniontech.com>
2021-03-03 11:04:16 +0800

    fix: 修复录音过程中关闭应用显示录音文件已删除
     修复录音过程中关闭应用显示录音文件已删除
    
    Log: 修复录音过程中关闭应用显示录音文件已删除
    
    Bug: https://pms.uniontech.com/zentao/bug-view-65040.html
    Change-Id: I9e18b76fc666b77511df341ad1f756488b456bce

--- src/views/vnotemainwindow.cpp
+++ src/views/vnotemainwindow.cpp
@@ -45,6 +45,7 @@
 #include "db/vnotefolderoper.h"
 #include "db/vnoteitemoper.h"
 #include "db/vnotedbmanager.h"
+#include "db/vnotesaferoper.h"
 
 #include "dbus/dbuslogin1manager.h"
 
@@ -959,12 +960,15 @@ void VNoteMainWindow::onFinshRecord(const QString &voicePath, qint64 voiceSize)
             safer.folder_id = currentNote->folderId;
             safer.note_id = currentNote->noteId;
             safer.path = voicePath;
-
-            VNoteDataSafefyManager::instance()->doSafer(safer);
+            if (!stateOperation->isAppQuit()) {
+                VNoteDataSafefyManager::instance()->doSafer(safer);
+            } else { //需要关闭应用时,同步更新数据库
+                VNoteSaferOper saferOper;
+                saferOper.rmSafer(safer);
+            }
         } else {
             qCritical() << "UnSafe get currentNote data is null! Dangerous!!!";
         }
-    } else {
     }
     setSpecialStatus(RecordEnd);
 
