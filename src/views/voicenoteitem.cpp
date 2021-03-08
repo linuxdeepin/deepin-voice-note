3234c960fa4dc8326060fd4c88a8da75d2764346
UT000454 <liuyanga@uniontech.com>
2021-03-08 16:13:02 +0800

    chore: debain规范copyright+license修改
     debain规范copyright+license修改
    
    Log: debain规范copyright+license修改
    Change-Id: I4562f28c3f31caad8d67478affcbdfcca36c10ea

--- src/views/voicenoteitem.cpp
+++ src/views/voicenoteitem.cpp
@@ -1,4 +1,4 @@
-﻿/*
+/*
 * Copyright (C) 2019 ~ 2020 Uniontech Software Technology Co.,Ltd.
 *
 * Author:     liuyanga <liuyanga@uniontech.com>
--- src/widgets/vnoterightmenu.h
+++ src/widgets/vnoterightmenu.h
@@ -1,3 +1,24 @@
+/*
+* Copyright (C) 2019 ~ 2020 Uniontech Software Technology Co.,Ltd.
+*
+* Author:     ningyuchuang <ningyuchuang@uniontech.com>
+*
+* Maintainer: ningyuchuang <ningyuchuang@uniontech.com>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #pragma once
 
 #include <DMenu>
@@ -19,9 +40,10 @@ protected:
     //初始化信号槽连接
     void initConnections();
     //处理鼠标move事件
-    void mouseMoveEvent(QMouseEvent *eve)override;
+    void mouseMoveEvent(QMouseEvent *eve) override;
     //处理鼠标release事件
-    void mouseReleaseEvent(QMouseEvent *eve)override;
+    void mouseReleaseEvent(QMouseEvent *eve) override;
+
 private:
     QPoint m_touchPoint;
     bool m_moved {false};
@@ -32,7 +54,4 @@ signals:
     //触摸释放信号
     void menuTouchReleased();
 private slots:
-
 };
-
-
