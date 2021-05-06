49eb239e137fd6a6171324d580b82164e26c296c
ut000454 <liuyanga@uniontech.com>
2021-05-06 16:53:01 +0800

    chore: 添加ut测试用例
     添加ut测试用例
    
    Log: 添加ut测试用例
    Change-Id: I44fcd73a7c46e85499f88fcfe9b0c76afa7c8e16

---
+++ tests/src/task/ut_vndatasafertask.h
@@ -0,0 +1,11 @@
+#ifndef UT_VNDATASAFERTASK_H
+#define UT_VNDATASAFERTASK_H
+#include<gtest/gtest.h>
+
+class ut_vndatasafertask_test : public ::testing::Test
+{
+public:
+    ut_vndatasafertask_test();
+};
+
+#endif // UT_VNDATASAFERTASK_H
