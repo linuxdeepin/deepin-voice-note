290f066befef58f4d1e7c421292d6916d37841f8
muyuankai <muyuankai@uniontech.com>
2022-09-06 15:21:28 +0800

    fix: 开源合规整改
    开源合规整改
    
    Log: 开源合规整改

--- tests/src/task/ut_exportnoteworker.cpp
+++ tests/src/task/ut_exportnoteworker.cpp
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2022 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 #include "ut_exportnoteworker.h"
 #include "task/exportnoteworker.h"
 #include "common/vnotedatamanager.h"
