0a66bed1b2c4945921f101ef4c8a6720b6db360a
ut001910 <muyuankai@uniontech.com>
2025-06-06 14:56:54 +0800

    fix: Assist AI in starting up
    Assist AI in starting up
    
    Bug: https://pms.uniontech.com/bug-view-315587.html
    Log: Assist AI in starting up

--- src/task/vnmainwnddelayinittask.cpp
+++ src/task/vnmainwnddelayinittask.cpp
@@ -5,6 +5,8 @@
 
 #include "vnmainwnddelayinittask.h"
 #include "views/vnotemainwindow.h"
+#include <QDBusInterface>
+#include <QDBusReply>
 
 /**
  * @brief VNMainWndDelayInitTask::VNMainWndDelayInitTask
@@ -23,6 +25,17 @@ VNMainWndDelayInitTask::VNMainWndDelayInitTask(VNoteMainWindow *pMainWnd, QObjec
 void VNMainWndDelayInitTask::run()
 {
     if (nullptr != m_pMainWnd) {
+        // 直接尝试调用 DBus 接口Add commentMore actions
+        QDBusInterface aiInterface("com.iflytek.aiassistant",
+                                 "/aiassistant/deepinmain",
+                                 "org.freedesktop.DBus.Introspectable");
+
+        QDBusReply<QString> reply = aiInterface.call("Introspect");
+        if (reply.isValid()) {
+            qInfo() << "com.iflytek.aiassistant service is available.";
+        } else {
+            qInfo() << "Failed to connect to com.iflytek.aiassistant: " << reply.error().message();
+        }
         //Delay initialize work
         m_pMainWnd->initDelayWork();
     }
